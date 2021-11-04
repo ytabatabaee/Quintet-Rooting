9118f4423528506603c9c377aa1de8329df6c911
ytabatabaee <yasamin.tabatabaee@gmail.com>
2021-11-04 11:27:18 -0500

    sketch of the code

---
+++ rooting.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import argparse
+import treeswift
+
+
+def main(args):
+  # compute gene tree distribution
+  # score the distribution
+  # return the rooted tree
+
+
+def root(u_distribution):
+
+
+def score(r, u_distribution):
+
+
+def parse_args():
+
+
+if __name__ == "__main__":
+    main(parse_args())
