fdcf995780bd687a8d440978f0db6ac29920ff80
Tabatabaee <syt3@golubh4.campuscluster.illinois.edu>
2021-11-25 10:51:13 -0600

    stripped genes - all conditions

---
+++ strip_trees.py
@@ -0,0 +1,15 @@
+import os
+import dendropy
+import argparse
+import glob
+
+genes_per_model_condition = glob.glob('avian_dataset/*.f200')
+
+if not os.path.exists('data/avian_dataset/'):
+     os.makedirs('data/avian_dataset/')
+
+for gene_tree_path in genes_per_model_condition:
+    print(gene_tree_path)
+    gene_trees = dendropy.TreeList.get(path=gene_tree_path, schema='newick', suppress_edge_lengths=True)
+    gene_trees.write_to_path(dest='data/' + gene_tree_path + '.stripped.tre', schema='newick', suppress_edge_lengths=True,
+                                        suppress_internal_node_labels=True)
