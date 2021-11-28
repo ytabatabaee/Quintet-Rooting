b29821aadd8dadaf9f745e00df37a156a1316ee6
ytabatabaee <yasamin.tabatabaee@gmail.com>
2021-11-28 10:51:52 -0600

    astral results

--- run_astral.py
+++ run_astral.py
@@ -3,25 +3,39 @@ import glob
 import subprocess
 import dendropy
 
-gene_tree_list = glob.glob('data/avian_genes_mapped*.tre')
+model_condition = 'avian-2X-1000-500-all' # only change this
+dataset_path = 'data/avian_dataset_final/extracted_quintets/'
+model_list = glob.glob(dataset_path + model_condition + '/gene_trees_mapped*.tre')
+output_dir = 'data/avian_dataset_final/astral_trees/'
 
-count = 0
+correct_topology_count = 0
+avg_rf_dist = 0
 
-for gene_tree_path in gene_tree_list:
-    indices_string = ''.join(c for c in gene_tree_path if c.isdigit())
-    species_tree_path = 'data/species_tree_mapped' + indices_string + '.tre'
-    output_path = 'data/species_tree_astral' + indices_string + '.tre'
-    print(species_tree_path)
-    #print(output_path)
+for item in model_list:
+    indices_string = ''.join(c for c in item.split('/')[-1] if c.isdigit())
+    true_species_tree_path = dataset_path + 'species_tree_mapped' + indices_string + '.tre'
+    gene_tree_path = dataset_path + model_condition + '/gene_trees_mapped' + indices_string + '.tre'
+    output_path = output_dir + model_condition + '/astral_species_tree' + indices_string + '.tre'
+
+    if not os.path.exists(output_dir + model_condition):
+        os.makedirs(output_dir + model_condition)
+
     cmd = 'java -jar Astral/astral.5.7.8.jar -i' + gene_tree_path + ' -o ' + output_path + ' 2>out.log'
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     out, err = p.communicate()
     tns = dendropy.TaxonNamespace()
-    species_tree_toplogy = dendropy.Tree.get(path=species_tree_path, schema='newick',
+    species_tree_toplogy = dendropy.Tree.get(path=true_species_tree_path, schema='newick',
                                         rooting="force-unrooted", taxon_namespace=tns)
     astral_tree_toplogy = dendropy.Tree.get(path=output_path, schema='newick',
                                         rooting="force-unrooted", taxon_namespace=tns, suppress_edge_lengths=True)
-    if dendropy.calculate.treecompare.symmetric_difference(species_tree_toplogy, astral_tree_toplogy) == 0:
-        count += 1
+    d = dendropy.calculate.treecompare.symmetric_difference(species_tree_toplogy, astral_tree_toplogy)
+    avg_rf_dist += d
+    correct_topology_count += 1 * (d == 0)
 
-print(count/len(gene_tree_list))
+
+data_size = len(model_list)
+
+print("Percentage of tests where the infered tree had the correct topology:")
+print(correct_topology_count/data_size*100)
+print("Average RF distance (not normalized, i.e. fp+fn)")
+print(avg_rf_dist/data_size)
