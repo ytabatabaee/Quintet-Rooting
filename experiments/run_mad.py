3a9589d3ff43b74f12b1264cf730e252229636ec
ytabatabaee <yasamin.tabatabaee@gmail.com>
2021-12-01 11:18:16 -0600

    update exps for mad, minvar

--- experiments/run_mad.py
+++ experiments/run_mad.py
@@ -3,14 +3,18 @@ import glob
 import subprocess
 import dendropy
 
-model_condition = 'avian-0_5X-1000-500' # only change this
-dataset_path = 'data/avian_dataset/extracted_quintets/'
+def rf_distance(t1, t2):
+    t1.encode_bipartitions()
+    t2.encode_bipartitions()
+    return dendropy.calculate.treecompare.symmetric_difference(t1, t2)
+
+model_condition = 'avian-0_5X-1000-500-all' # only change this
+dataset_path = '../data/avian_dataset/extracted_quintets/'
 model_list = glob.glob(dataset_path + model_condition + '/gene_trees_mapped*.tre')
-output_dir = 'data/avian_dataset/mad_trees/'
-print(len(model_list))
+output_dir = '../data/avian_dataset/mad_trees/'
 
 count = 0
-avg_distance = 0
+avg_clade_distance = 0
 
 for item in model_list:
     indices_string = ''.join(c for c in item.split('/')[-1] if c.isdigit())
@@ -34,16 +38,16 @@ for item in model_list:
                                         rooting="force-rooted", taxon_namespace=tns)
     mp_species_tree = dendropy.Tree.get(path='temp.tre.rooted', schema='newick',
                                         rooting="force-rooted", taxon_namespace=tns, suppress_edge_lengths=True)
-    d = dendropy.calculate.treecompare.symmetric_difference(true_species_tree, mp_species_tree)
-    avg_distance += d
+    d = rf_distance(true_species_tree, mp_species_tree)
+    avg_clade_distance += d
     count += int(d == 0)
+
+data_size = len(model_list)
+
 
 print("Test count")
 print(data_size)
-print("Percentage of tests where the infered tree had the correct topology:")
-print(correct_topology_count/data_size*100)
+print("Percentage of tests correctly rooted:")
+print(count/data_size*100)
 print("Average RF distance (not normalized, i.e. fp+fn)")
-print(avg_rf_dist/data_size)
-
-print(count/len(model_list))
-print(avg_distance/len(model_list))
+print(avg_clade_distance/data_size)
--- experiments/run_minvar.py
+++ experiments/run_minvar.py
@@ -3,17 +3,16 @@ import glob
 import subprocess
 import dendropy
 
-model_condition = 'avian-1X-1000-500'
-dataset_path = 'data/avian_dataset/extracted_quintets/'
+model_condition = 'avian-1X-1000-500-all'
+dataset_path = '../data/avian_dataset/extracted_quintets/'
 model_list = glob.glob(dataset_path + model_condition + '/gene_trees_mapped*.tre')
 method = 'MV' # only this and the model condition need to be changed afterwards
-output_dir = 'data/avian_dataset/' + method + '_trees/'
-print(len(model_list))
+output_dir = '../data/avian_dataset/' + method + '_trees/'
 
 count = 0
-avg_distance = 0
+avg_clade_distance = 0
 
-for item in model_list:
+for item in model_list[:10]:
     indices_string = ''.join(c for c in item.split('/')[-1] if c.isdigit())
     species_tree_with_lengths = dataset_path + 'species_tree_mapped_with_lengths' + indices_string + '.tre'
     true_species_tree_path = dataset_path + 'species_tree_mapped' + indices_string + '.tre'
@@ -32,8 +31,14 @@ for item in model_list:
     mp_species_tree = dendropy.Tree.get(path=output_path, schema='newick',
                                         rooting="force-rooted", taxon_namespace=tns, suppress_edge_lengths=True)
     d = dendropy.calculate.treecompare.symmetric_difference(true_species_tree, mp_species_tree)
-    avg_distance += d
+    avg_clade_distance += d
     count += int(d == 0)
 
-print(count/len(model_list))
-print(avg_distance/len(model_list))
+data_size = len(model_list[:10])
+
+print("Test count")
+print(data_size)
+print("Percentage of tests correctly rooted:")
+print(count/data_size*100)
+print("Average RF distance (not normalized, i.e. fp+fn)")
+print(avg_clade_distance/data_size)
