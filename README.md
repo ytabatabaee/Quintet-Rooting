4985aa2022bfa3eb0dddfddc59b39ce546706bd4
ytabatabaee <yasamin.tabatabaee@gmail.com>
2022-02-09 20:22:59 -0600

    sampling modes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Quintet Rooting
 
-Quintet Rooting is a method for rooting species trees from multi-locus datasets that uses phylogenetic invariants and inequalities under the multi-species coalescent (MSC) model to identify the position of the root in an unrooted tree. Quintet Rooting is statistically consistent under the MSC and is especially useful for multi-locus datasets with gene tree discordance due to incomplete lineage sorting (ILS). Quintet Rooting scores different rootings of a given unrooted species tree according to the distribution of unrooted quintets (i.e. 5-leaf trees) induced by a given set of gene trees, and returns the rooting with the minimum overall cost across all or a subset of quintets. 
+Quintet Rooting is a method for rooting species trees from multi-locus datasets that uses phylogenetic invariants and inequalities under the multi-species coalescent (MSC) model to identify the position of the root in an unrooted tree. Quintet Rooting is statistically consistent under the MSC and is especially useful for multi-locus datasets with gene tree discordance due to incomplete lineage sorting (ILS). Quintet Rooting scores different rootings of a given unrooted species tree according to the distribution of unrooted quintets (i.e. 5-leaf trees) induced by a given set of gene trees, and returns the rooting with the minimum overall cost across all or a subset of quintets.
 
 ## Contents
 - [Dependencies](#dependencies)
@@ -18,14 +18,14 @@ Quintet Rooting is implemented in Python 3. It was developed and tested in Pytho
 If you have Python 3 and pip, you can use `pip install -r requirements.txt` to install the other dependencies.
 
 ## Usage Instructions
-Quintet Rooting must be run in a directory containing the `rooted_quintet_indices.npy` file and files in the `./topologies` directory. We recommend that you clone the repository and run `quintet-rooting.py` in the base directory.
+Quintet Rooting must be run in a directory containing the `rooted_quintet_indices.npy` file and files in the `./topologies` directory. We recommend that you clone the repository and run `quintet_rooting.py` in the base directory.
 
 ### Rooting an unrooted species tree
 **Input:** A file containing a species tree topology and a file containing a set of gene trees, both in newick format
 
 **Output:** A file containing the rooted species tree in newick format
 ```
-$ python3 quintet-rooting.py -t <species-topology.tre> -g <input-genes.tre> -o <output-tree.tre> 
+$ python3 quintet_rooting.py -t <species-topology.tre> -g <input-genes.tre> -o <output-tree.tre>
 ```
 **Arguments**
 - **Required**
@@ -43,10 +43,10 @@ $ python3 quintet-rooting.py -t <species-topology.tre> -g <input-genes.tre> -o <
 **Example**
 The `example` directory contains two example sets, one containing a 40-taxon avian species tree with 1000 genes and the other containing a 10-taxon mammalian species tree with 200 genes. The commands below show different modes of running Quintet Rooting on these data:
 ```
-$ python3 quintet-rooting.py -t ./example/avian-species-40.tre -g ./example/avian-genes-40.tre -o ./example/avian-rooted-40.tre -sm LE
+$ python3 quintet_rooting.py -t ./example/avian-species-40.tre -g ./example/avian-genes-40.tre -o ./example/avian-rooted-40.tre -sm LE
 ```
 ```
-$ python3 quintet-rooting.py -t ./example/mammalian-species-10.tre -g ./example/mammalian-genes-10.tre -o ./example/mammalian-rooted-10.tre -sm TC -c MC
+$ python3 quintet_rooting.py -t ./example/mammalian-species-10.tre -g ./example/mammalian-genes-10.tre -o ./example/mammalian-rooted-10.tre -sm TC -c MC
 ```
 ### Quintet Sampling Method
 Quintet Rooting can run with three different sampling methods. The default version runs in O(n<sup>5</sup>k), where n is the number of taxa and k is the number of gene trees, and exhaustively scores all quintets. The `TC` version scores a subset of quintets that guarantee to cover all triplets and runs in O(n<sup>3</sup>k). The `LE` version uses a linear mapping of edges in the unrooted topology to quintets and and runs in O(nk). The default version is generally more accurate, although we recommond using the `LE` version for datasets with more than 20 species, as the others will take longer to run. All three sampling methods are statistically consistent under the multi-species coalescent model and have provided reasonably close accuracy under simulations.
--- quintet_rooting.py
+++ quintet_rooting.py
@@ -5,12 +5,14 @@ import dendropy
 import numpy as np
 import time
 import itertools
+import random
 
 
 def main(args):
     species_tree_path = args.speciestree
     gene_tree_path = args.genetrees
     output_path = args.outputtree
+    sampling_method = args.samplingmethod
 
     # reading gene tree and species tree topology files
     tns = dendropy.TaxonNamespace()
@@ -26,37 +28,87 @@ def main(args):
 
     rooted_candidates = get_all_rooted_trees(species_tree_toplogy)
     r_score = np.zeros(len(rooted_candidates))
-    print(len(rooted_candidates))
 
     taxon_set = [t.label for t in tns]
-    all_quintet_taxa = list(itertools.combinations(taxon_set, 5))
-    print(len(all_quintet_taxa))
+    sample_quintet_taxa = []
+    if sampling_method == 'd':
+        sample_quintet_taxa = list(itertools.combinations(taxon_set, 5))
+    elif sampling_method == 'tc':
+        sample_quintet_taxa = triplet_cover_sample(taxon_set)
+    elif sampling_method == 'le':
+        sample_quintet_taxa = linear_quintet_encoding_sample(species_tree_toplogy, taxon_set)
+
     for i in range(len(rooted_candidates)):
         r = rooted_candidates[i]
-        print(r.as_string(schema='newick'))
-        for q_taxa in all_quintet_taxa:
-            print(q_taxa)
+        for q_taxa in sample_quintet_taxa:
             start_time = time.time()
             cost = extract_quintets(r, gene_trees, q_taxa, unrooted_quintets_base, rooted_quintets_base, tns, rooted_quintet_indices)
-            print(time.time() - start_time)
-            print(cost)
             r_score[i] += cost
-        print(r_score[i])
-    print(len(all_quintet_taxa))
     min_idx = np.argmin(r_score)
-    for i in range(len(rooted_candidates)):
-        print(rooted_candidates[i].as_string(schema='newick'))
-        print(r_score[i])
-    print("The best tree is:")
-    print(min_idx)
-    print(rooted_candidates[min_idx])
+    rooted_candidates[min_idx].write_to_path(dest=output_path, schema='newick')
     return
 
+
+def triplet_cover_sample(taxon_set):
+    sample_quintet_taxa = []
+    all_triplet_taxa = list(itertools.combinations(taxon_set, 3))
+    for trip in all_triplet_taxa:
+        trip_prime = random.sample([x for x in taxon_set if x not in trip], 2)
+        sample_quintet_taxa.append(tuple(list(trip) + list(trip_prime)))
+    return sample_quintet_taxa
+
+
+def linear_quintet_encoding_sample(unrooted_tree, taxon_set):
+    sample_quintet_taxa = []
+    tree = dendropy.Tree(unrooted_tree)
+    for edge in tree.preorder_edge_iter():
+        seed_node = tree.seed_node
+        try:
+            if edge.is_leaf():
+                quintet = []
+                tri_node = edge.tail_node
+                tree.reroot_at_node(tri_node, update_bipartitions=True)
+                tri_partition_taxa = []
+                for child in tree.seed_node.child_node_iter():
+                    partition = []
+                    for c in child.leaf_nodes():
+                        partition.append(c.taxon.label)
+                    tri_partition_taxa.append(partition)
+                if len(tri_partition_taxa) > 2:
+                    for partition in tri_partition_taxa:
+                        quintet.extend(random.sample(partition, 1))
+                    quintet.extend(random.sample([x for x in taxon_set if x not in quintet], 5 - len(quintet)))
+                    sample_quintet_taxa.append(tuple(quintet))
+                tree.reroot_at_node(seed_node, update_bipartitions=True)
+
+            elif edge.is_internal():
+                quintet = []
+                adj_edges  = edge.get_adjacent_edges()
+                tree.reroot_at_edge(edge, update_bipartitions=True)
+                four_partition_taxa = []
+                for e in adj_edges:
+                    partition = []
+                    for c in e.head_node.leaf_nodes():
+                        partition.append(c.taxon.label)
+                    four_partition_taxa.append(partition)
+                for i in range(len(four_partition_taxa)):
+                    for j in range(i+1, len(four_partition_taxa)):
+                        if set(four_partition_taxa[i]).issubset(set(four_partition_taxa[j])):
+                            four_partition_taxa[j] = list(set(four_partition_taxa[j]) - set(four_partition_taxa[i]))
+                four_partition_taxa = [p for p in four_partition_taxa if p != []]
+                if len(four_partition_taxa) > 2:
+                    for partition in four_partition_taxa:
+                        quintet.extend(random.sample(partition, 1))
+                    quintet.extend(random.sample([x for x in taxon_set if x not in quintet], 5 - len(quintet)))
+                    sample_quintet_taxa.append(tuple(quintet))
+                tree.reroot_at_node(seed_node, update_bipartitions=True)
+        except Exception as ex:
+            continue
+    return sample_quintet_taxa
 
 
 def extract_quintets(species_tree, gene_trees, q_taxa, unrooted_quintets_base, rooted_quintets_base, tns, rooted_quintet_indices):
     subtree = species_tree.extract_tree_with_taxa_labels(labels=q_taxa, suppress_unifurcations=True)
-    print(subtree.as_string(schema='newick'))
     quintets = [dendropy.Tree.get(data=map_taxonnamespace(str(q), q_taxa)+';', schema='newick', rooting='force-unrooted', taxon_namespace=tns) for q in unrooted_quintets_base]
     rooted_quintets = [dendropy.Tree.get(data=map_taxonnamespace(str(q), q_taxa)+';', schema='newick', rooting='force-rooted', taxon_namespace=tns) for q in rooted_quintets_base]
     # compute ui values
@@ -72,10 +124,8 @@ def extract_quintets(species_tree, gene_trees, q_taxa, unrooted_quintets_base, r
     idx = -1
     for i in range(len(rooted_quintets)):
         if dendropy.calculate.treecompare.symmetric_difference(rooted_quintets[i], subtree) == 0:
-            print(rooted_quintets[i].as_string(schema='newick'))
             idx = i
             break
-    print("rooted tree index, " + str(idx+1))
     tree_type = None
     if idx < 60:
         tree_type = 'c'
@@ -104,21 +154,23 @@ def get_all_rooted_trees(unrooted_tree):
             rooted_candidates.append(dendropy.Tree(tree))
         except:
             continue
-    # removing duplicates (by default, it returns 2n-2 trees)
+    # removing duplicates
     for i1 in range(len(rooted_candidates)):
         for i2 in range(i1+1, len(rooted_candidates)):
             if dendropy.calculate.treecompare.symmetric_difference(rooted_candidates[i1], rooted_candidates[i2]) == 0:
                 rooted_candidates.pop(i2)
                 break
+    for t in rooted_candidates:
+        print(t)
     return rooted_candidates
 
 
 def invariant_metric(a, b):
-    return np.abs(a - b)# / (a + b)
+    return np.abs(a - b)
 
 
-def inequality_metric(a, b): # it should be a < b
-    return (a-b) * (a > b)#a - b# penalty when a > b
+def inequality_metric(a, b):
+    return (a-b) * (a > b)
 
 
 def cost(u, indices, type):
@@ -126,9 +178,6 @@ def cost(u, indices, type):
     inequality_score = 0
     equivalence_classes = []
     inequality_classes = []
-    #print(indices)
-    #for i in indices:
-    #    print(u[i])
     if type == 'c':
         equivalence_classes = [[0], [1], [2], [3, 12], [4, 11], [5, 8], [6, 7, 9, 10, 13, 14]]
         inequality_classes = [[0, 1], [0, 3], [1, 4], [3, 4], [4, 6], [2, 1], [2, 5], [5, 4]] # [a, b] -> a > b, a and b are cluster indices
@@ -139,39 +188,37 @@ def cost(u, indices, type):
         equivalence_classes = [[0], [1, 2], [3, 12], [7, 10], [4, 5, 6, 8, 9, 11, 13, 14]]
         inequality_classes = [[0, 1], [0, 2], [0, 3], [1, 4], [2, 4], [3, 4]]
     for c in equivalence_classes:
-        #print([indices[i]+1 for i in c]) #** important
         inclass_distance = 0
         for i in range(len(c)):
             for j in range(len(c)):
                 inclass_distance += invariant_metric(u[indices[c[i]]], u[indices[c[j]]])
-                #print(str(u[indices[c[i]]]) + ' - ' + str(u[indices[c[j]]]))
-        invariant_score += inclass_distance/(len(c))#(len(c)-1)
-    #invariant_score = invariant_score# / len(equivalence_classes)
+        invariant_score += inclass_distance/(len(c))
 
     for ineq in inequality_classes: #distance between clusters
         outclass_distance = 0
-        #print("check " + str(ineq[0]) + " > " + str(ineq[1]))
         for i in equivalence_classes[ineq[0]]:
             for j in equivalence_classes[ineq[1]]:
-                #print("check " + str(u[indices[i]]) + " > " + str(u[indices[j]]) + " with score " +  str(inequality_metric(u[indices[j]], u[indices[i]])))
                 outclass_distance += inequality_metric(u[indices[j]], u[indices[i]])
         inequality_score += outclass_distance/(len(equivalence_classes[ineq[0]]))#*len(equivalence_classes[ineq[1]]))
-    #inequality_score = 0# / len(inequality_classes)
-
     return invariant_score + inequality_score
-
 
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-t", "--speciestree", type=str, help="Input unrooted species tree",
+    parser.add_argument("-t", "--speciestree", type=str, help="input unrooted species tree in newick format",
                         required=True, default=None)
 
-    parser.add_argument("-g", "--genetrees", type=str, help="Input gene trees",
+    parser.add_argument("-g", "--genetrees", type=str, help="input gene trees in newick format",
                         required=True, default=None)
 
-    parser.add_argument("-o", "--outputtree", type=str, help="Output rooted species tree",
+    parser.add_argument("-o", "--outputtree", type=str, help="output file containing a rooted species tree",
                         required=True, default=None)
+
+    parser.add_argument("-sm", "--samplingmethod", type=str, help="quintet sampling method (TC for triplet cover, LE for linear encoding)",
+                        required=False, default='d')
+
+    parser.add_argument("-c", "--cost", type=str, help="cost function (MC for minimal constraints)",
+                        required=False, default='d')
 
     return parser.parse_args()
 
