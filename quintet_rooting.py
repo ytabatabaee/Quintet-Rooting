0e43f2209d2b1d6cf89873fa537cb1ac79892d92
ytabatabaee <yasamin.tabatabaee@gmail.com>
2022-05-27 17:04:46 -0500

    optimizing the implemenetation

--- quintet_rooting.py
+++ quintet_rooting.py
@@ -17,19 +17,22 @@ def main(args):
 
     # reading gene tree and species tree topology files
     tns = dendropy.TaxonNamespace()
-    species_tree_toplogy = dendropy.Tree.get(path=species_tree_path, schema='newick',
+    tns_base = dendropy.TaxonNamespace()
+    unrooted_species_tree_topology = dendropy.Tree.get(path=species_tree_path, schema='newick',
                                          taxon_namespace=tns, rooting="force-unrooted", suppress_edge_lengths=True)
     gene_trees = dendropy.TreeList.get(path=gene_tree_path, schema='newick', taxon_namespace=tns, rooting="force-unrooted")
-    unrooted_quintets_base = dendropy.TreeList.get(path='topologies/quintets.tre', schema='newick')
-    rooted_quintets_base = dendropy.TreeList()
+    unrooted_quintets_base = dendropy.TreeList.get(path='topologies/quintets.tre', taxon_namespace=tns_base, schema='newick')
+    rooted_quintets_base = dendropy.TreeList(taxon_namespace=tns_base)
     rooted_quintets_base.read(path='topologies/caterpillar.tre', schema='newick', rooting="default-rooted")
     rooted_quintets_base.read(path='topologies/pseudo_caterpillar.tre', schema='newick', rooting="default-rooted")
     rooted_quintets_base.read(path='topologies/balanced.tre', schema='newick', rooting="default-rooted")
     rooted_quintet_indices = np.load('rooted_quintet_indices.npy')
 
-    rooted_candidates = get_all_rooted_trees(species_tree_toplogy)
+    # search space of rooted trees
+    rooted_candidates = get_all_rooted_trees(unrooted_species_tree_topology)
     r_score = np.zeros(len(rooted_candidates))
 
+    # set of sampled quintets
     taxon_set = [t.label for t in tns]
     sample_quintet_taxa = []
     if len(taxon_set) == 5 or sampling_method == 'd':
@@ -37,18 +40,46 @@ def main(args):
     elif sampling_method == 'tc':
         sample_quintet_taxa = triplet_cover_sample(taxon_set)
     elif sampling_method == 'le':
-        sample_quintet_taxa = linear_quintet_encoding_sample(species_tree_toplogy, taxon_set)
+        sample_quintet_taxa = linear_quintet_encoding_sample(unrooted_species_tree_topology, taxon_set)
     elif sampling_method == 'rl':
         sample_quintet_taxa = random_linear_sampling(taxon_set)
 
+
+    start = time.time()
+
+    # preprocessing
+    quintet_scores = np.zeros((len(sample_quintet_taxa), 7))
+    quintet_unrooted_indices = np.zeros(len(sample_quintet_taxa), dtype=int)
+    quintets_r_all = []
+
+    for j in range(len(sample_quintet_taxa)):
+        q_taxa = sample_quintet_taxa[j]
+        quintets_u = [dendropy.Tree.get(data=map_taxonnamespace(str(q), q_taxa)+';', schema='newick', rooting='force-unrooted', taxon_namespace=tns) for q in unrooted_quintets_base]
+        quintets_r = [dendropy.Tree.get(data=map_taxonnamespace(str(q), q_taxa)+';', schema='newick', rooting='force-rooted', taxon_namespace=tns) for q in rooted_quintets_base] # you don't need all of this
+        subtree_u = unrooted_species_tree_topology.extract_tree_with_taxa_labels(labels=q_taxa, suppress_unifurcations=True)
+        quintet_tree_dist = gene_tree_distribution(gene_trees, q_taxa, quintets_u)
+        quintet_unrooted_indices[j] = get_quintet_unrooted_index(subtree_u, q_taxa, quintets_u)
+        quintet_scores[j] = compute_cost_rooted_quintets(quintet_tree_dist, quintet_unrooted_indices[j], rooted_quintet_indices)
+        quintets_r_all.append(quintets_r)
+
+    #print('preprocessing time', time.time() - start)
+
+    start = time.time()
+
+    # computing scores
     for i in range(len(rooted_candidates)):
         r = rooted_candidates[i]
-        for q_taxa in sample_quintet_taxa:
-            start_time = time.time()
-            cost = extract_quintets(r, gene_trees, q_taxa, unrooted_quintets_base, rooted_quintets_base, tns, rooted_quintet_indices)
-            r_score[i] += cost
+        for j in range(len(sample_quintet_taxa)):
+            q_taxa = sample_quintet_taxa[j]
+            subtree_r = r.extract_tree_with_taxa_labels(labels=q_taxa, suppress_unifurcations=True)
+            r_idx = get_quintet_rooted_index(subtree_r, q_taxa, quintets_r_all[j], quintet_unrooted_indices[j])
+            r_score[i] += quintet_scores[j][r_idx]
+
     min_idx = np.argmin(r_score)
     rooted_candidates[min_idx].write_to_path(dest=output_path, schema='newick')
+    #print(r_score)
+
+    #print('scoring time', time.time() - start)
 
     if args.confidencescore:
         confidence_scores = (np.max(r_score) - r_score)/np.sum(np.max(r_score) - r_score)
@@ -57,6 +88,7 @@ def main(args):
             for i in tree_ranking_indices:
                 fp.write(str(rooted_candidates[i]) + ';\n')
                 fp.write(str(confidence_scores[i]) + '\n')
+
 
 def random_linear_sampling(taxon_set):
     n = len(taxon_set)
@@ -120,35 +152,75 @@ def linear_quintet_encoding_sample(unrooted_tree, taxon_set):
             continue
     return sample_quintet_taxa
 
+# ADR Table 5
+u2r_mapping = np.array([[0, 1, 58, 59, 66, 75, 104],
+                       [2, 3, 52, 53, 63, 78, 103],
+                       [4, 5, 46, 47, 60, 87, 102],
+                       [6, 7, 56, 57, 69, 76, 101],
+                       [8, 9, 40, 41, 64, 81, 100],
+                       [10, 11, 34, 35, 61, 90, 99],
+                       [12, 13, 50, 51, 70, 79, 95],
+                       [14, 15, 38, 39, 67, 82, 94],
+                       [16, 17, 28, 29, 62, 93, 96],
+                       [18, 19, 44, 45, 71, 86, 88],
+                       [20, 21, 32, 33, 68, 85, 91],
+                       [22, 23, 26, 27, 65, 84, 97],
+                       [24, 25, 54, 55, 72, 77, 98],
+                       [30, 31, 48, 49, 73, 80, 92],
+                       [36, 37, 42, 43, 74, 83, 89]])
 
-def extract_quintets(species_tree, gene_trees, q_taxa, unrooted_quintets_base, rooted_quintets_base, tns, rooted_quintet_indices):
-    subtree = species_tree.extract_tree_with_taxa_labels(labels=q_taxa, suppress_unifurcations=True)
-    quintets = [dendropy.Tree.get(data=map_taxonnamespace(str(q), q_taxa)+';', schema='newick', rooting='force-unrooted', taxon_namespace=tns) for q in unrooted_quintets_base]
-    rooted_quintets = [dendropy.Tree.get(data=map_taxonnamespace(str(q), q_taxa)+';', schema='newick', rooting='force-rooted', taxon_namespace=tns) for q in rooted_quintets_base]
-    # compute ui values
-    u_count = np.zeros(len(quintets))
+
+# score the 7 rooted variants of a quintet
+def compute_cost_rooted_quintets(u_distribution, u_idx, rooted_quintet_indices):
+    rooted_tree_indices = u2r_mapping[u_idx]
+    costs = np.zeros(7)
+    for i in range(7):
+        idx = rooted_tree_indices[i]
+        unlabeled_topology = idx_2_unlabeled_topology(idx)
+        indices = rooted_quintet_indices[idx]
+        costs[i] = cost(u_distribution, indices, unlabeled_topology)
+    return costs
+
+
+def idx_2_unlabeled_topology(idx):
+    if idx < 60:
+        return 'c'
+    elif idx >= 60 and idx < 75:
+        return 'p'
+    elif idx >= 75 and idx < 105:
+        return 'b'
+    return None
+
+
+def gene_tree_distribution(gene_trees, q_taxa, quintets_u):
+    u_count = np.zeros(len(quintets_u))
     for g in gene_trees:
         g_subtree = g.extract_tree_with_taxa_labels(labels=q_taxa, suppress_unifurcations=True)
-        for i in range(len(quintets)):
-            if dendropy.calculate.treecompare.symmetric_difference(quintets[i], g_subtree) == 0:
+        for i in range(len(quintets_u)):
+            if dendropy.calculate.treecompare.symmetric_difference(quintets_u[i], g_subtree) == 0:
                 u_count[i] += 1
                 break
     u_distribution = u_count / len(gene_trees)
-    # get the index of the rooted tree
-    idx = -1
-    for i in range(len(rooted_quintets)):
-        if dendropy.calculate.treecompare.symmetric_difference(rooted_quintets[i], subtree) == 0:
-            idx = i
+    return u_distribution
+
+
+def get_quintet_unrooted_index(subtree_u, q_taxa, quintets_u):
+    idx_u = -1
+    for i in range(len(quintets_u)):
+        if dendropy.calculate.treecompare.symmetric_difference(quintets_u[i], subtree_u) == 0:
+            idx_u = i
             break
-    tree_type = None
-    if idx < 60:
-        tree_type = 'c'
-    elif idx >= 60 and idx < 75:
-        tree_type = 'p'
-    elif idx >= 75 and idx < 105:
-        tree_type = 'b'
-    indices = rooted_quintet_indices[idx]
-    return cost(u_distribution, indices, tree_type)
+    return idx_u
+
+
+def get_quintet_rooted_index(subtree_r, q_taxa, quintets_r, u_idx):
+    idx_r = -1
+    for i in range(7):
+        idx = u2r_mapping[u_idx][i]
+        if dendropy.calculate.treecompare.symmetric_difference(quintets_r[idx], subtree_r) == 0:
+            idx_r = i
+            break
+    return idx_r
 
 
 def map_taxonnamespace(s, map):
@@ -213,6 +285,7 @@ def cost(u, indices, type):
                 outclass_distance += inequality_metric(u[indices[j]], u[indices[i]])
         inequality_score += outclass_distance/(len(equivalence_classes[ineq[0]]))
     return invariant_score + inequality_score
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
